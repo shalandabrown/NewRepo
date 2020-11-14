88453671dc88f49e8d21230143bb499dffe27298
Shala <s00043039@acad.tri-c.edu>
2020-11-14 11:05:17 -0500

    LAB 4

---
+++ LAB 4/LAB 4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LAB_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+// a control variable, the control variable's initial value, 
+// the control variable's increment that applied during each iteration of the loop
+// the loop-continuation condition that determines if looping should continue
+// while and for can each be used for either iteration type; when the for statement
+// begins executing, the control variable counter is declared; next the program
+// checks the loop-continuation condition; after executing the loop's body, the program
+// increments counter in the expression ++counter; then the program performs the 
+// loop-continuation test again to determine whether to continue with the loop's next
+// interation; the process continues until the members have been displayed and the
+// counters's value becomes the first statement.
+//  the do....while statement tests the loop-continuation condition after executing
+// the loop's body; therefore the body always executes at least once; if the condition
+// is true, the loop continues from the body statement; if false the loop terminates; and
+// the app continues with the next statement after the loop
+for (int counter = i; counter <= 100; ++counter)
+{
+    Console.Write($" {counter}  ");
+}
+Console.WriteLine()
+    if ((i % 2) == 0) // it's even
+{
+    Console.WriteLine("Even==0");
+}
+// please enter a temperature
+for (int temp = T; counter <= 90; ++counter)
+{
+    Console.WriteLine("<10 Polar Bear, <20 Penguin <40 Moose, <50 Reindeer,
+    < 60 Deer, < 70 Turtle, < 80 Lion, < 90 Fish, default Bug"");
+}
+
+int i = 10;
+while (i < 21)
+{
+    Console.WriteLine(i);
+}
+for (int i = 0; i < 101; i++)
+{
+    Console.WriteLine(i);
+    Console.WriteLine("i % <101");
+}
+
+
+
+
+
+
+
+
+
+
